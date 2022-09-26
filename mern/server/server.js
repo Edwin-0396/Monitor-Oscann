7f850bc08b98074d3ebda27eb9e37a681014d4f8
Edwin-9603 <egc4611@gmail.com>
2022-09-26 15:57:30 +0000

    ne wupdates

--- mern/server/server.js
+++ mern/server/server.js
@@ -3,11 +3,27 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+//var cron = require('node-cron');
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record")); //REVIEW
 // get driver connection
 const dbo = require("./db/conn");
+
+
+//var task = cron.schedule('5 * * * *', () =>  {
+/*  axios 
+  let db_connect = dbo.getDb("employees");
+ db_connect
+   .collection("records")
+   .find({})
+   .toArray(function (err, result) {
+     if (err) throw err;
+     res.json(result);
+   });
+}, {
+  scheduled: false
+});*/
 
 app.listen(port, () => {
   // perform a database connection when server starts

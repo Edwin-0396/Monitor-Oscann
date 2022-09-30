437c7281cc6a35ead102f296940a5f9086a97ecc
Edwin-9603 <egc4611@gmail.com>
2022-09-30 16:11:21 +0000

    new move

--- Monitor_Oscann_Dashboard/server/db/conn.js
+++ Monitor_Oscann_Dashboard/server/db/conn.js
@@ -14,36 +14,3 @@ module.exports = {
     });
   },
 };
-
-
-/*
-console.log("hH")
-
-run()
-async function run(){
-  const Oscan1 = new oscann({
-    name: "Risa",
-    position: "Centro",
-    level: "Low",})
-  await Oscan1.save().then(() => {console.log("Oscann Stored")})
-  const Oscan2 = new oscann({
-    name: "Quindio",
-    position: "Right",
-    level: "Midium",})
-  await Oscan2.save().then(() => {console.log("Oscann Stored")})
-  let myquery = { _id: "6333a226c8b5231d4b4c4064" };
-  oscann.findOne(myquery, function (err, result) {
-    if (err) throw err;
-    console.log(result);
-  })
-  let myobj = {
-    name: "Caldas",
-    position: "Centro",
-    level: "High",
-  };
-  oscann.insertMany(myobj, function (err, res) {
-    if (err) throw err;
-    console.log(res)
-  });
-
-}*/

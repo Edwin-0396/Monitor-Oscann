138f2c4e7491cdce5314f84c7a6cc542734ddb35
Edwin-9603 <egc4611@gmail.com>
2022-09-30 15:19:21 +0000

    new updates

--- Rest-Api-ExpressMongoDB/models/model.js
+++ Rest-Api-ExpressMongoDB/models/model.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose');
 
 const dataSchema = new mongoose.Schema({
-    name: {
-        required: true,
-        type: String
-    },
-    age: {
-        required: true,
-        type: Number
-    }
-})
+    _id: mongoose.Schema.Types.ObjectId,
+    name: String
+}, { collection: 'Oscann' })
 
-module.exports = mongoose.model('Data', dataSchema)
+module.exports = mongoose.model('Oscann', dataSchema)

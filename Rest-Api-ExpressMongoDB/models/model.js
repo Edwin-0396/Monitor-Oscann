d653982bf008bd53101c3fd3251bac8398a116fc
DanielN24 <capdaniel_98@outlook.com>
2022-09-30 15:48:14 +0000

    moved directory

--- Rest-Api-ExpressMongoDB/models/model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const dataSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: String
-}, { collection: 'Oscann' })
-
-module.exports = mongoose.model('Oscann', dataSchema)

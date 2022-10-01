3da6bb560ef363c67b86d9aff5551a44dae2d8e0
DanielN24 <capdaniel_98@outlook.com>
2022-10-01 04:01:37 +0000

    gran avance.. los felicito a todos <3

--- Monitor_Oscann_Dashboard/client/src/sidebar.js
+++ Monitor_Oscann_Dashboard/client/src/sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {CDBSidebarContent, CDBSidebarHeader} from 'cdbreact';
 import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
@@ -7,14 +7,38 @@ import {IoMdDisc} from 'react-icons/io';
 import { Link } from 'react-router-dom';
 
 function Sidebar() {
+    
+    const URL = 'http://localhost:4600/api/getAll'
+    const [records, setRecords] = useState([]);
+
+  
+
+    useEffect(() => {
+        async function getRecords() {
+            const response = await fetch(URL);
+
+            if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            window.alert(message);
+            return;
+            }
+
+            const records = await response.json();
+            setRecords(records);
+        }
+
+        getRecords();
+
+        return;
+    }, [records.length]);
     const [isOpen, setIsOpen] = useState()
-     if (!isOpen) {
-        return (
-            <i className="fa fa-bars fa-large" style={{ margin: 30, cursor: 'pointer', color: 'black' }} onClick={function () {
-                setIsOpen(true)
-            }}></i>
-        )
-    }
+        if (!isOpen) {
+            return (
+                <i className="fa fa-bars fa-large" style={{ margin: 30, cursor: 'pointer', color: 'black' }} onClick={function () {
+                    setIsOpen(true)
+                }}></i>
+            )
+        }
     return (
         <div>
             <ProSidebar>
@@ -26,56 +50,23 @@ function Sidebar() {
                     </CDBSidebarHeader>
                     <CDBSidebarContent>
                         <Menu>
+                        {records.map((loop) => (
                             <div className='Menu'>
-                                <SubMenu title="Distributor 1" icon= {<IoMdDisc className='myIcon' style={{color: 'red'}} />}>
-                                    <SubMenu title="Hospital 1" icon= {<IoMdDisc  className='mySubIcon' style={{color: 'green'}} />}>
-                                        <div><MenuItem><Link to='/oscan'>Oscan 1</Link></MenuItem></div>
-                                        <MenuItem>Oscan 2 </MenuItem>
-                                        <MenuItem>Oscan 3 <IoMdDisc className='mySubIcon' /></MenuItem>
+                                <SubMenu title={loop.nombre_distribuidor} icon= {<IoMdDisc className='myIcon' style={{color: 'red'}} />}>
+                                {loop.Distribuidores_hospitalarios.map((loop1) => (
+                                    <SubMenu title={loop1.DH_name} icon= {<IoMdDisc  className='mySubIcon' style={{color: 'green'}} />}>
+                                        {loop1.Hospitales.map((loop2) => (
+                                        <SubMenu title={loop2.hospital_name} icon= {<IoMdDisc  className='mySubIcon' style={{color: 'green'}} />}>
+                                            {loop2.Oscann.map((loop3) => (
+                                                <div ><MenuItem icon= {<IoMdDisc className='iconoOscanns' style={{color: 'red'}} />}><Link to='/oscan'>{loop3.NAME} </Link></MenuItem></div>
+                                            ))}
+                                        </SubMenu>
+                                        ))}    
                                     </SubMenu>
-                                    <SubMenu title="Hospital 2" icon= {<IoMdDisc className='mySubIcon' style={{color: 'red'}} />} >
-                                        <MenuItem>Oscan 1 <IoMdDisc className='mySubIcon' style={{color: 'red'}} /></MenuItem>
-                                        <MenuItem>Oscan 2 <IoMdDisc className='mySubIcon' style={{color: 'red'}} /></MenuItem>
-                                        <MenuItem>Oscan 3 <IoMdDisc className='mySubIcon' style={{color: 'red'}} /></MenuItem>
-                                    </SubMenu>
-                                </SubMenu>
-                                <SubMenu title="Distributor 2" icon= {<IoMdDisc className='myIcon' style={{color: 'green'}} />} >
-                                    <SubMenu title="Hospital 1" icon= {<IoMdDisc className='mySubIcon' style={{color: 'green'}} />} >
-                                        <MenuItem>Oscan 1 </MenuItem>
-                                        <MenuItem>Oscan 2 </MenuItem>
-                                        <MenuItem>Oscan 3 </MenuItem>
-                                    </SubMenu>
-                                    <SubMenu title="Hospital 2">
-                                        <MenuItem>Oscan 1 </MenuItem>
-                                        <MenuItem>Oscan 2 </MenuItem>
-                                        <MenuItem>Oscan 3 </MenuItem>
-                                    </SubMenu>
-                                </SubMenu>
-                                <SubMenu title="Distributor 3" icon= {<IoMdDisc className='myIcon' style={{color: 'green'}} />} >
-                                    <SubMenu title="Hospital 1">
-                                        <MenuItem>Oscan 1 </MenuItem>
-                                        <MenuItem>Oscan 2 </MenuItem>
-                                        <MenuItem>Oscan 3 </MenuItem>
-                                    </SubMenu>
-                                    <SubMenu title="Hospital 2">
-                                        <MenuItem>Oscan 1 </MenuItem>
-                                        <MenuItem>Oscan 2 </MenuItem>
-                                        <MenuItem>Oscan 3 </MenuItem>
-                                    </SubMenu>
-                                </SubMenu>
-                                <SubMenu title="Distributor 4" icon= {<IoMdDisc className='myIcon' style={{color: 'green'}} />} >
-                                    <SubMenu title="Hospital 1">
-                                        <MenuItem>Oscan 1 </MenuItem>
-                                        <MenuItem>Oscan 2 </MenuItem>
-                                        <MenuItem>Oscan 3 </MenuItem>
-                                    </SubMenu>
-                                    <SubMenu title="Hospital 4">
-                                        <MenuItem>Oscan 1 </MenuItem>
-                                        <MenuItem>Oscan 2 </MenuItem>
-                                        <MenuItem>Oscan 3 </MenuItem>
-                                    </SubMenu>
+                                    ))}
                                 </SubMenu>
                             </div>
+                        ))}
                         </Menu>
                     </CDBSidebarContent>
                 </div>

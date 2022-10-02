a75ff7bb50805821e19928d79c46b4405234c26d
Edwin-9603 <egc4611@gmail.com>
2022-10-02 21:14:30 +0000

    new updates

--- Monitor_Oscann_Dashboard/server/db/models/model_oscann.js
+++ Monitor_Oscann_Dashboard/server/db/models/model_oscann.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const dataSchema = new mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	ID: String
-}, { timestamps: true}, { collection: 'oscanns' })
+}, { timestamps: true}, { collection: 'Oscanns' })
 
-module.exports = mongoose.model('oscanns', dataSchema)
+module.exports = mongoose.model('Oscanns', dataSchema)
